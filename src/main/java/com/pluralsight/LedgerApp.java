0882d98c7310f824c5164b3ee8038dd33e6865fd
nayeliespinoza-dev <nayeliespinoza003@gmail.com>
2025-10-16 18:22:27 -0700

    Added methods for deposit, payment, and displaying ledger

--- src/main/java/com/pluralsight/LedgerApp.java
+++ src/main/java/com/pluralsight/LedgerApp.java
@@ -48,13 +48,79 @@ public class LedgerApp {
         System.out.println("2. Make Payment (Debit)");
         System.out.println("3. Display Ledger");
         System.out.println("4. Exit");
-        System.out.println("Select an option: ");
+        System.out.print("Select an option: ");
     }
 
-    private void addDeposit() {}
+// Method adding a deposit option
 
-    private void makePayment() {}
+    private void addDeposit() {
+        System.out.println("\n--- Add Deposit ---");
+        System.out.println("Description: ");
+        String description = scanner.nextLine().trim();
 
-    private void displayLedger() {}
+        System.out.println("Vendor: ");
+        String vendor = scanner.nextLine().trim();
 
-}
+        double amount = readPositiveAmount("Amount (positive): ");
+
+        Transaction t = new Transaction(LocalDate.now(), LocalTime.now(), description, vendor, amount);
+        transactions.add(t);
+        System.out.printf("Deposit of $%.2f added.\n", amount);
+    }
+
+// Method for making a payment
+
+    private void makePayment() {
+        System.out.println("\n--- Make Payment (Debit)---");
+        System.out.print("Description: ");
+        String description = scanner.nextLine().trim();
+
+        System.out.println("Vendor: ");
+        String vendor = scanner.nextLine().trim();
+
+        double amount = readPositiveAmount("Amount (positive) -> will be recorded as a payment: ");
+
+        Transaction t = new Transaction(LocalDate.now(), LocalTime.now(), description, vendor, -amount);
+        transactions.add(t);
+        System.out.printf("Payment of $%.2f recorded.\n", amount);
+    }
+
+    private double readPositiveAmount(String prompt) {
+        double amount = 0.0;
+        while (true) {
+            System.out.print(prompt);
+            String input = scanner.nextLine().trim();
+            try {
+                amount = Double.parseDouble(input);
+                if (amount <= 0) {
+                    System.out.println("Please enter an amount greater than 0.");
+                    continue;
+                }
+                return amount;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Try again (example: 125.50.");
+            }
+        }
+    }
+
+    private void displayLedger() {
+        System.out.println("\n--- Ledger ---");
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions yet.");
+            return;
+        }
+
+        System.out.printf("%-20s %-8s | %-20s | %-15s | %10s\n", "Date", "Time", "Description", "Vendor", "Amount");
+        System.out.println("-------------------------------------------------------------------------------------------");
+
+
+        double balance = 0.0;
+        for (Transaction t : transactions) {
+            System.out.println(t.toString());
+            balance += t.getAmount();
+        }
+
+        System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.printf("Current Balance: $%.2f\n", balance);
+        }
+    }
