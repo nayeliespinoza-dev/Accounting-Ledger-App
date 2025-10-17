20babe4c81cd8cb23b74247735e7d42a37f128c6
nayeliespinoza-dev <nayeliespinoza003@gmail.com>
2025-10-16 17:20:12 -0700

    Added transaction class for ledger entries.

--- src/main/java/com/pluralsight/LedgerApp.java
+++ src/main/java/com/pluralsight/LedgerApp.java
@@ -1,11 +1,4 @@
 package com.pluralsight;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class LedgerApp {
-    private Arraylist<Transaction> transaction = new ArrayList<>();
-    private Scanner scanner = new Scanner(System.in);
 }
